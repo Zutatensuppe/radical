d63b9c5564d9fe9ef1d4e71710a3c52cb2133c9c
Zutatensuppe <oo.para@gmail.com>
2019-06-30 12:35:35 +0200

    adding collection_\get_deep function

---
+++ test/unit/CollectionTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace zutatensuppe\radical\test\unit;
+
+use PHPUnit\Framework\TestCase;
+use zutatensuppe\radical\collection_;
+
+class CollectionTest extends TestCase
+{
+    /**
+     * @dataProvider getDeepDataProvider
+     */
+    public function testGetDeep($mixed, $path, $expected): void
+    {
+        self::assertSame($expected, collection_\get_deep($mixed, $path));
+    }
+
+    public function getDeepDataProvider()
+    {
+        return [
+            [
+                (object) ['hi' => ['oki' => (object)['doki' =>'as']]],
+                ['hi', 'oki', 'doki'],
+                'as'
+            ],
+            [
+                (object) ['hi' => ['oki' => (object)['doki' =>'as']]],
+                ['hi', 'oki', 'c'],
+                null
+            ],
+        ];
+    }
+}
